292ee5f99deabb2dc0bf6600e2c270e71a50f5f8
Hector <hcontrerascorvacho@gmail.com>
2024-09-03 21:07:19 -0400

    BBDD

--- src/components/MascotaForm.jsx
+++ src/components/MascotaForm.jsx
@@ -1,5 +1,22 @@
 import React, { useState } from 'react';
 import { getDatabase, ref, push } from 'firebase/database';
+import { initializeApp } from 'firebase/app';
+//import { getDatabase, ref, set } from 'firebase/database';
+
+
+// Configura tu aplicación de Firebase con tus claves
+const firebaseConfig = {
+  apiKey: "AIzaSyAyDdP0QjxPDvcB9DdGju9Xl7ouGvjZ2hI",
+  authDomain: "mascotas-8fa86.firebaseapp.com",
+  projectId: "mascotas-8fa86",
+  storageBucket: "mascotas-8fa86.appspot.com",
+  messagingSenderId: "960763048480",
+  appId: "1:960763048480:web:3feda522a90efea1390245"
+};
+
+// Inicializa la aplicación de Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);  
 
 const MascotaForm = () => {
   const [formData, setFormData] = useState({
@@ -59,33 +76,61 @@ const MascotaForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="name">Nombre:</label>
-      <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
-      
-      <label htmlFor="raza">Raza:</label>
-      <input type="text" id="raza" name="raza" value={formData.raza} onChange={handleChange} required />
-      
-      <label htmlFor="tam">Tamaño:</label>
-      <input type="text" id="tam" name="tam" value={formData.tam} onChange={handleChange} required />
+    <form onSubmit={handleSubmit} className="max-w-md mx-auto mt-8 p-6 bg-white rounded-lg shadow-md">
+    <div className="mb-4">
+      <label htmlFor="name" className="block text-gray-700 text-sm font-bold mb-2">Nombre:</label>
+      <input type="text" id="name" name="name" placeholder="Pon el nombre de tu amigo peludo" value={formData.name} onChange={handleChange} required 
+             className="bg-white shadow-lg border-2 appearance-none  rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+    </div>  
+    
+    <div className="mb-4">
+      <label htmlFor="raza" className="block text-gray-700 text-sm font-bold mb-2">Raza:</label>
+      <input type="text" id="raza" name="raza" placeholder="Raza de tu mascota" value={formData.raza} onChange={handleChange} required 
+             className="bg-white shadow-lg border-2  appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+    </div>
+    
+    <div className="mb-4">
+      <label htmlFor="tam" className="block text-gray-700 text-sm font-bold mb-2">Tamaño:</label>
+      <input type="text" id="tam" name="tam"   placeholder="Pequeño, mediano, grande" value={formData.tam} onChange={handleChange} required 
+             className="bg-white shadow-lg border-2  appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+    </div>
 
-      <label htmlFor="micro">Microchip:</label>
-      <input type="text" id="micro" name="micro" value={formData.micro} onChange={handleChange} required />
+    <div className="mb-4">
+      <label htmlFor="micro" className="block text-gray-700 text-sm font-bold mb-2">Microchip:</label>
+      <input type="text" id="micro" name="micro" placeholder="Número de microchip" value={formData.micro} onChange={handleChange} required 
+             className="bg-white shadow-lg border-2  appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+    </div>
 
-      <label htmlFor="marcas">Marcas:</label>
-      <input type="text" id="marcas" name="marcas" value={formData.marcas} onChange={handleChange} required />
+    <div className="mb-4">
+      <label htmlFor="marcas" className="block text-gray-700 text-sm font-bold mb-2">Marcas:</label>
+      <input type="text" id="marcas" name="marcas" placeholder='Descripción de cualquier marca distintiva' value={formData.marcas} onChange={handleChange} required 
+             className="bg-white shadow-lg border-2  appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+    </div>
 
-      <label htmlFor="fecha_de_perdida">Fecha de pérdida:</label>
-      <input type="date" id="fecha_de_perdida" name="fecha_de_perdida" value={formData.fecha_de_perdida} onChange={handleChange} required />
+    <div className="mb-4">
+      <label htmlFor="fecha_de_perdida" className="block text-gray-700 text-sm font-bold mb-2">Fecha de pérdida:</label>
+      <input type="date" id="fecha_de_perdida" name="fecha_de_perdida" value={formData.fecha_de_perdida} onChange={handleChange} required 
+             className="bg-white shadow-lg border-2  appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+    </div>
 
-      <label htmlFor="fotografia">Fotografía:</label>
-      <input type="file" id="fotografia" name="fotografia" accept="image/*" onChange={handleFotografiaChange} />
+    <div className="mb-4">
+      <label htmlFor="fotografia" className="block text-gray-700 text-sm font-bold mb-2">Fotografía:</label>
+      <input type="file" id="fotografia" name="fotografia" accept="image/*" onChange={handleFotografiaChange} 
+             className="bg-white shadow-lg border-2  appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+    </div>
 
-      <label htmlFor="descripcion">Descripción:</label>
-      <textarea id="descripcion" name="descripcion" value={formData.descripcion} onChange={handleChange} required></textarea>
-      
-      <button type="submit">Enviar</button>
-    </form>
+    <div className="mb-6">
+      <label htmlFor="descripcion" className="block text-gray-700 text-sm font-bold mb-2">Descripción:</label>
+      <textarea id="descripcion" placeholder='Detalles sobre tu mascota' name="descripcion" value={formData.descripcion} onChange={handleChange} required 
+                className="bg-white shadow-lg border-2  appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline h-32"></textarea>
+    </div>
+    
+    <div className="flex items-center justify-between">
+      <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+        Enviar
+      </button>
+    </div>
+  </form>
   );
 };
 
